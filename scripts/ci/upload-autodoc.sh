506108f5a571e865b4ea823c230d6fd997f203a5
Ivan Gromakovskii <gie1994@gmail.com>
2020-10-27 16:03:58 +0300

    [#18] Upload contract documentation from CI
    Problem: we generate contract documentation on CI, but
    accessing it from CI artifacts is not convenient.
    
    Solution: upload it to the repo (to a separate branch) using
    our existing script to do that.

---
+++ scripts/ci/upload-autodoc.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2019-2020 Tocqueville Group
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -e -o pipefail
+
+git config --global user.email "hi@serokell.io"
+git config --global user.name "CI autodoc generator"
+git remote remove auth-origin 2> /dev/null || :
+git remote add auth-origin https://serokell:$AUTODOC_TOKEN@github.com/tqtezos/baseDAO.git
+git fetch
+
+our_branch="$BUILDKITE_BRANCH"
+doc_branch="autodoc/$our_branch"
+sha=$(git rev-parse --short HEAD)
+git checkout origin/$doc_branch
+git checkout -B $doc_branch
+git merge -X theirs origin/$our_branch -m "Merge $our_branch"
+mv tmp/BaseDAO.md BaseDAO.md
+git add BaseDAO.md
+git commit --allow-empty -m "Documentation update for $sha"
+git push --set-upstream auth-origin $doc_branch
+git checkout @{-2}
