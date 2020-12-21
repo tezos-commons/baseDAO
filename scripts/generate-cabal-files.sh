7b61611f03528ecb31008b09c43c3b45ee419c19
Roman Melnikov <roman.melnikov@serokell.io>
2020-12-21 10:53:46 +0300

    [#65] Add script for cabal files generation
    Problem: We want to have a way for consistent cabal files production.
    
    Solution: Add script that calls stack2cabal, which uses dedicated stack
    snapshot.

---
+++ scripts/generate-cabal-files.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2020 Tocqueville Group
+#
+# SPDX-License-Identifier: LicenseRef-MIT-TQ
+
+# This script generates cabal.project, cabal.project.freeze and *.cabal
+# files which can be used in order to build baseDAO using cabal
+
+set -e
+
+[ -f cabal.project ] && rm cabal.project
+[ -f cabal.project.freeze ] && rm cabal.project.freeze
+
+mkdir -p stack2cabal
+stack install stack2cabal --resolver snapshot-stack2cabal.yaml --local-bin-path stack2cabal
+./stack2cabal/stack2cabal
+rm -rf stack2cabal
+echo "cabal.project, cabal.project.freeze and *.cabal files are successfully generated"
